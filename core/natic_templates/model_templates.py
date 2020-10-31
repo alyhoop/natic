b51d1346b5ccc05b9d58cc1e247c850e204d801a
vramirez209 <vramirez209@gmail.com>
2020-10-31 01:38:26 -0400

    updated docstrings

--- core/natic_templates/model_templates.py
+++ core/natic_templates/model_templates.py
@@ -2,7 +2,16 @@ import uuid
 from django.db import models
 
 
+"""This file will prove basic django abstract model templates to alleviate duplicating the development efforts. 
+Abstract models do not create tables in the databse.See https://docs.djangoproject.com/en/3.1/topics/db/models/#abstract-base-classes for more info.
+Templates will be created based on the models purpose and audience (end-user,system level).
+Good pratice is to create UUID in every model."""
+
+
 class StandardModel(models.Model):
+
+    """created_date: generates the date a record was created.
+    last_modified: updates whenever a record is modified."""
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_date = models.DateTimeField(auto_now_add=True)
